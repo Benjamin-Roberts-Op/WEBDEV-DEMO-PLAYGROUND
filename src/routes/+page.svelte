6e8b3ea1298bc2b68b2e959f7418cf35e9ed4f5c
Benjamin Roberts (1000006827) <ROBEBP1@student.op.ac.nz>
2026-09-25 10:43:47 +1200

    test commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,34 @@
  let src = '/images/image.gif'
 </script>
 
-<img src="{dancingCat}" alt="">
+<header>
+<h1>We Learn Svelte</h1>
+<nav><ul>
+    <li><a href="/">Home</a></li>
+    <li><a href="/">About</a></li>
+    <li><a href="/">Contact</a></li>
+</ul></nav>
+</header>
+
+<main>
+<article>
+    <figure></figure>
+    <section>
+        <h2>Boring Card</h2>
+        <ul>
+            <li>one</li>
+            <li>two</li>
+            <li>three</li>
+        </ul>
+    </section>
+</article>
+</main>
+
+<footer>
+
+</footer>
+
+    <img src="{dancingCat}" alt="">
 <h1>Hello {name.toUpperCase()}!</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
