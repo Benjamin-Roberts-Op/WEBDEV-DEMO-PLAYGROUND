e40ea3b887893d06fc1fc6cb3cc58b7aa91e931c
Benjamin Roberts (1000006827) <ROBEBP1@student.op.ac.nz>
2026-09-25 10:25:46 +1200

    innitial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+ import dancingCat from '$lib/assets/images/image.gif'
+ let name = 'Svelte'
+ let src = '/images/image.gif'
+</script>
+
+<img src="{dancingCat}" alt="">
+<h1>Hello {name.toUpperCase()}!</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<style>
+p{
+    color: goldenrod;
+}
+</style>
